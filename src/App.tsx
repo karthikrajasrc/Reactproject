97c40b02b167b2a4297b3f0f84df1989ab218579
karthikrajasrc <karthikrajasrc@gmail.com>
2026-02-09 19:27:05 +0530

    props in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-import './App.css'
+type props = {
+  name: string,
+  age: number
+}
 
-function App() {
+const Header = ({name, age} : props) => {
   return (
-    <h1>Hello World!!</h1>
+    <>
+      <h1>Hello, {name.slice(0, 1).toUpperCase() + name.slice(1,)}! you are {age} years old!!</h1>
+    </>
   )
 }
 
-export default App
+
+const App = () => {
+  let name: string = "karthik";
+  let age:number = 22;
+
+  return (
+    <>
+      <Header name={name} age={age}  />
+    </>
+  )
+}
+export default App;
