289b0d0d0c7520151712ee27093912165976f7c8
karthikrajasrc <karthikrajasrc@gmail.com>
2026-02-10 23:31:53 +0530

    using useState hook in react

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
-import Header from "./Header"
+import { useState } from "react"
+
 
 const App = () => {
 
-  const getData = (message: string) => {
-    console.log(message);
+  const [Counter, setCounter] = useState({ count: 0, history: []});
+
+  const handleIncrease = () => {
+    setCounter({ count: Counter.count + 1, history: [...Counter.history, "I"] })
   }
 
+  const handleDecrease = () => {
+    setCounter({ count: Counter.count - 1, history: [...Counter.history, "D"] });
+  }
 
   return (
-      <Header getData = {getData} />
+    <div>
+      <h1>Count: {Counter.count} </h1>
+      <h2>History:{Counter.history.join(" ")} </h2>
+      <button onClick = {handleIncrease}>Increase</button> &nbsp;
+      <button onClick = {handleDecrease}>Decrease</button>
+    </div>
   )
 }
 
